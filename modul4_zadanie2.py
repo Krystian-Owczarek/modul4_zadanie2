7dc9cf22237cd927672114d7be2feaf426d992fa
Krystian Owczarek <k.owczarek4848@gmail.com>
2022-05-29 18:45:48 +0200

    Take float instead of int

--- modul4_zadanie2.py
+++ modul4_zadanie2.py
@@ -1,5 +1,5 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 '''
 Prosty kalkulator który pozwala wykonywać działa na dwóch liczbach.
@@ -7,33 +7,33 @@ Prosty kalkulator który pozwala wykonywać działa na dwóch liczbach.
 
 def calculation_choice(number): #Funkcja która wykonuje odpowiednie działanie w zależności od wyboru użytkownika
     if number == 1:
-        logging.debug(f'Dodaję {first_number} do {second_number}')
+        logging.info(f'Dodaję {first_number} do {second_number}')
         print(first_number + second_number)
     elif number == 2:
-        logging.debug(f'Odejmuję {first_number} od {second_number}')
+        logging.info(f'Odejmuję {first_number} od {second_number}')
         print(first_number - second_number)
     elif number == 3:
-        logging.debug(f'Mnożę {first_number} razy {second_number}')
+        logging.info(f'Mnożę {first_number} razy {second_number}')
         print(first_number * second_number)
     elif number == 4:
-        logging.debug(f'Dzielę {first_number} przez {second_number}')
+        logging.info(f'Dzielę {first_number} przez {second_number}')
         print(first_number / second_number)
 
 
 def choose_again(number): #Funkcja która pozwala na ponowny wybór opcji działania w przypadku braku wybrania opcji od 1 do 4
   while number < 1 or number > 4:
     logging.warning('Wybór nie odpowiada opcją. Dokonaj wyboru ponownie: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
-    number = int(input())
+    number = float(input())
   return number
 
 if __name__ == '__main__':
 
     print('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
-    choice_number = int(input())
+    choice_number = float(input())
     choice_number = choose_again(choice_number)
     print('Podaj pierwszą liczbę:')
-    first_number = int(input())
+    first_number = float(input())
     print('Podaj drugą liczbę')
-    second_number = int(input())
+    second_number = float(input())
     print('Wynik dodawania to: ')
     calculation_choice(choice_number)
