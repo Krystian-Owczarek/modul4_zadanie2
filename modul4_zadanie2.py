20e902da95358dfad4143368687e43f3c28ae0d6
Krystian Owczarek <k.owczarek4848@gmail.com>
2022-05-29 18:37:13 +0200

    Add documentation and Logging

--- modul4_zadanie2.py
+++ modul4_zadanie2.py
@@ -1,23 +1,31 @@
 import logging
+logging.basicConfig(level=logging.DEBUG)
 
+'''
+Prosty kalkulator który pozwala wykonywać działa na dwóch liczbach.
+'''
 
-
-def calculation_choice(number):
+def calculation_choice(number): #Funkcja która wykonuje odpowiednie działanie w zależności od wyboru użytkownika
     if number == 1:
+        logging.debug(f'Dodaję {first_number} do {second_number}')
         print(first_number + second_number)
     elif number == 2:
+        logging.debug(f'Odejmuję {first_number} od {second_number}')
         print(first_number - second_number)
     elif number == 3:
+        logging.debug(f'Mnożę {first_number} razy {second_number}')
         print(first_number * second_number)
     elif number == 4:
+        logging.debug(f'Dzielę {first_number} przez {second_number}')
         print(first_number / second_number)
 
-def choose_again(number):
+
+def choose_again(number): #Funkcja która pozwala na ponowny wybór opcji działania w przypadku braku wybrania opcji od 1 do 4
   while number < 1 or number > 4:
-    print('Wybór nie odpowiada opcją. Dokonaj wyboru jeszcze raz: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
+    logging.warning('Wybór nie odpowiada opcją. Dokonaj wyboru ponownie: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
     number = int(input())
   return number
-  
+
 if __name__ == '__main__':
 
     print('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
