c9c34d0596c7b37ae4c432f68a72861f73b02fe4
Krystian Owczarek <k.owczarek4848@gmail.com>
2022-05-29 21:48:00 +0200

    Remove typos

--- modul4_zadanie2.py
+++ modul4_zadanie2.py
@@ -8,26 +8,26 @@ Prosty kalkulator który pozwala wykonywać działania na dwóch liczbach.
 def calculation_choice(number): #Funkcja która wykonuje odpowiednie działanie w zależności od wyboru opcji użytkownika
     if number == 1:
         logging.info(f'Dodaję {first_number} do {second_number}')
-        print(first_number + second_number)
+        print(f'Wynik to: {first_number + second_number}')
     elif number == 2:
         logging.info(f'Odejmuję {first_number} od {second_number}')
-        print(first_number - second_number)
+        print(f'Wynik to: {first_number - second_number}')
     elif number == 3:
         logging.info(f'Mnożę {first_number} razy {second_number}')
-        print(first_number * second_number)
+        print(f'Wynik to: {first_number * second_number}')
     elif number == 4:
         logging.info(f'Dzielę {first_number} przez {second_number}')
-        print(first_number / second_number)
+        print(f'Wynik to: {first_number / second_number}')
 
 def choose_again(number): #Funkcja która pozwala na ponowny wybór opcji działania w przypadku wpisania innej liczby niż od 1 do 4
 
     while number < 1 or number > 4:
 
         try: #Pętla która wypisuje treść błędu dopóki wpisane dane nie będą odpowiadać wymaganym (float)
-            logging.warning('Wybór nie odpowiada opcją. Dokonaj wyboru ponownie: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
-            number = int(input())
+            logging.warning('Wybór nie jest liczbą od 1 do 4, spróbuj ponownie: \n1 Dodawanie, \n2 Odejmowanie, \n3 Mnożenie, \n4 Dzielenie: ')
+            number = int(input('Opcja '))
         except ValueError:
-            print('Wybór nie jest liczbą, spróbuj ponownie')
+            logging.warning('Wybór nie jest liczbą od 1 do 4, spróbuj ponownie')
 
     return number
 
@@ -37,10 +37,10 @@ if __name__ == '__main__':
 
     while True:
         try:
-            choice_number = int(input()) #Wybór opcji działania
+            choice_number = int(input('Opcja ')) #Wybór opcji działania
             break;
         except ValueError:
-            print('Wybór nie jest liczbą, spróbuj ponownie')
+            logging.warning('Wybór nie jest liczbą od 1 do 4, spróbuj ponownie')
             
     choice_number = choose_again(choice_number)
 
@@ -49,15 +49,15 @@ if __name__ == '__main__':
             first_number = float(input('Podaj pierwszą liczbę: '))
             break;
         except ValueError:
-            print('Dane nie jest liczbą, spróbuj ponownie')
+            logging.warning('Wybór nie jest liczbą, spróbuj ponownie')
 
     while True: #Pętla która wypisuje treść błędu dopóki wpisane dane nie będą odpowiadać wymaganym (float)
         try:
             second_number = float(input('Podaj drugą liczbę: '))
             break;
         except ValueError:
-            print('Dane nie jest liczbą, spróbuj ponownie')
+            logging.warning('Wybór nie jest liczbą, spróbuj ponownie')
 
-    print('Wynik dodawania to:')
     calculation_choice(choice_number)
+    
     
